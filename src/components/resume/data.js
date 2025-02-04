fcacb4ca62073faf952252181cc32ef6aed4d971
Idhamm15 <idhamtamvanz123@gmail.com>
2025-02-04 13:11:44 +0600

    Portofolio Websites

---
+++ src/components/resume/data.js
@@ -0,0 +1,66 @@
+export const experiences = [
+    {
+      title: "Web Developer",
+      company: "Waqfi Store",
+      period: "Apr 2024 - Present",
+      description: `As a Web Developer at Waqfi Store since April 2023, I have been responsible for building and maintaining our online store website using Laravel.
+      My work includes implementing core functionalities such as user authentication, product management, and order processing.
+      I integrated secure payment gateways and third-party services to enhance the shopping experience, optimized the website for performance and scalability, and ensured a seamless and fast user experience. 
+      I created responsive and user-friendly interfaces for various devices and collaborated with cross-functional teams including designers, marketers, and product managers to align with business goals. Additionally, 
+      I engaged in regular code reviews and iterative improvements to maintain high code quality and address emerging issues.`
+    },
+    {
+        title: "Backend Engineer",
+        company: "Ruang Guru",
+        period: "Feb 2024 - Jun 2024",
+        description: `During my tenure at Ruang Guru as a Backend Engineer from February 2024 to June 2024, I focused on learning the Go programming language to develop an integrated backend system powered by AI. 
+        I was responsible for designing and implementing AI-integrated backend services, creating scalable APIs for smooth communication between frontend and backend systems with PostgreSQL, optimizing backend processes for improved efficiency and reliability, 
+        collaborating with cross-functional teams to integrate AI features, and resolving complex technical issues to enhance system stability and user experience. This role provided me with valuable experience in utilizing Go for backend development and integrating advanced AI technologies into a comprehensive system.`
+    },
+    {
+      "title": "Panitia Invofest 2023 IT Division",
+      "company": "POLITEKNIK HARAPAN BERSAMA",
+      "period": "Okt 2023 - Okt 2023",
+      "description": "I contributed to the Invofest 2023 event as part of the IT Division, responsible for building a website for competition, seminar, and workshop registration using React and Firebase."
+    },
+    {
+      "title": "Web Developer",
+      "company": "Selapura Village",
+      "period": "Mar 2023 - May 2023",
+      "description": "I contributed to the development of the Selapura Village profile website, which was built collaboratively using native PHP. The website was tailored to meet the village's specific needs and included a dashboard feature for managing website content. My role involved creating and implementing various functionalities to ensure the site effectively served the village's requirements."
+    },
+    {
+      "title": "Frontend Developer",
+      "company": "PT Barrans Global Mandiri",
+      "period": "Sep 2022 - Oct 2022",
+      "description": "Continued working on an existing project from the company, utilizing React.js and Redux to enhance and maintain the frontend functionality."
+    },  
+    {
+      title: "Frontend Developer Bootcamp Batch III",
+      company: "Kawah Edukasi",
+      period: "Aug 2022 - Oct 2022",
+      description: `Frontend of Kawah Edukasi Bootcamp Project (August - September 2022)
+      Organizing a project with the team.
+    
+      ● Build front end only and use React Js.
+      ● CRUD book data, Admin auth, User view, and admin view.
+      ● And for the backend has been provided.`
+    }
+  
+  ];
+  
+  export const education = [
+    {
+      title: "Str.Kom, Informatic Engineering",
+      institution: "Politeknik Harapan Bersama",
+      period: "Year 2021 - Present",
+      description: `I am currently pursuing a Bachelor's degree in Informatics Engineering at Politeknik Harapan Bersama. 
+      Throughout my studies, I have gained a solid foundation in computer science principles, programming languages, and software development methodologies. 
+      I have worked on various projects, including developing web applications, mobile apps, and database management systems. 
+      My coursework has covered a wide range of topics such as algorithms, data structures, computer networks, cybersecurity, and artificial intelligence.
+      I have actively participated in several extracurricular activities, including coding competitions, hackathons, and tech seminars, which have enhanced my problem-solving skills and teamwork abilities. 
+      My academic journey has been marked by a commitment to continuous learning and adapting to new technologies, ensuring that I stay up-to-date with the latest advancements in the field.`
+    }
+    // Tambahkan pendidikan lain di sini
+  ];
+  
---
+++ src/data/data.js
@@ -0,0 +1,57 @@
+import anxiety from '../assets/project/2.png';
+import gravida from '../assets/project/5.png';
+import infovest from '../assets/project/4.png';
+import semedo from '../assets/project/3.png';
+import Selapura from '../assets/project/selapura.png';
+
+
+
+export const data = [
+  {
+    id: 1,
+    img: Selapura,
+    title: "Website Profile Desa Selapura",
+    desc: "",
+    link: "https://desaselapura.site",
+    git: "",
+  },
+  // {
+  //   id: 2,
+  //   img: infovest,
+  //   title: "SPK Pemilihan Course Programming",
+  //   link: "",
+  //   git: "",
+  // },
+  {
+    id: 3,
+    img: semedo,
+    title: "Detection Artefak Semedo Tegal",
+    desc: "Testing",
+    link: "https://semedo012.vercel.app/",
+    git: "",
+  },
+  {
+    id: 4,
+    img: infovest,
+    title: "Website Infovest 2023",
+    desc: "Testing",
+    link: "https://invofest2023.vercel.app/",
+    git: "",
+  },
+  {
+    id: 5,
+    img: gravida,
+    title: "Website Penghitung hari lahir",
+    desc: "Testing",
+    link: "https://gravida.my.id",
+    git: "https://github.com/Idhamm15/Gravida-Project.git",
+  },
+  {
+    id: 6,
+    img: anxiety,
+    title: "Website Edukasi Kesehatan",
+    desc: "Testing",
+    link: "https://anxietyally.vercel.app",
+    git: "",
+  }
+];
